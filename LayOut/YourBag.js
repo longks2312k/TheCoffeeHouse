0ca3456855aa2079824234d895d1fd9025679a06
long <longks2312k@gmail.com>
2021-08-12 10:28:30 +0700

    Begin

---
+++ LayOut/YourBag.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function YourBag() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
