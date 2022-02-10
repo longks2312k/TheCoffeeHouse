84d69be4f94a681ef82673173ceeaad8fa4a2f8a
longks2312k <longks2312k@gmail.com>
2022-02-10 16:22:08 +0700

    Bag

--- src/screens/SonOfOrder/Bag.js
+++ src/screens/SonOfOrder/Bag.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, FlatList, StyleSheet, StatusBar, TouchableOpacity, SafeAreaView, Image, Dimensions, ScrollView, ViewBase, TextInput } from 'react-native'
 import RnIcon from 'react-native-vector-icons/Ionicons';
+
 import RnIcon1 from 'react-native-vector-icons/MaterialCommunityIcons';
 import RnIcon2 from 'react-native-vector-icons/Fontisto';
 import RnIcon3 from 'react-native-vector-icons/FontAwesome5';
@@ -8,7 +9,6 @@ import RnIcon3 from 'react-native-vector-icons/FontAwesome5';
 import { useSelector, useDispatch } from "react-redux";
 
 // import RadioGroup from 'react-native-radio-buttons-group';
-
 // const radioButtonsData = [
 //   {
 //     id: '1',
@@ -28,15 +28,14 @@ import { useSelector, useDispatch } from "react-redux";
 //   },
 // ];
 
-function TotalPrice(price, tonggia) {
-	return Number(price * tonggia).toLocaleString('en-US');
-}
-
 export default function Bag({ navigation }) {
 
-	const dispatch = useDispatch();
+	// total
 	const [size,setSize] = useState(6000)
 	const productList = useSelector((store) => store.cartReducer.products);
+	function TotalPrice(price, tonggia) {
+		return Number(price * tonggia).toLocaleString('en-US');
+	}
 	const totalMoney = productList.reduce((acc, ele) => acc + Number(ele.price + size) * ele.quantity , 0)
 	const totalItem = productList.reduce((acc, ele) => acc + ele.quantity, 0)
 
@@ -48,6 +47,8 @@ export default function Bag({ navigation }) {
 	//   setSize(radioButtonsArray[1].sizePrice);
 	// };
 
+	// redux
+	const dispatch = useDispatch();
 	const onRemoveItem = (item) => () => {
 		dispatch({ type: 'REMOVE_CART', data: item })
 	}
@@ -62,22 +63,23 @@ export default function Bag({ navigation }) {
 		}
 	}
 
+	// render
 	const renderItem = ({ item }) => {
 		return (
-			<View style={{ flex: 1, marginVertical: 5,marginHorizontal:10, flexDirection: 'row', borderRadius: 8, backgroundColor: 'white', padding: 10, width: '95%', marginRight: 10 }}>
+			<View style={styles.viewItem}>
 				<Image
 					style={{ height: 100, flex:3, borderRadius: 5 }}
 					source={{ uri: item.image }}
 				/>
 				<View style={{ flex:5.8,}}>
-					<Text ellipsizeMode='tail' numberOfLines={1} style={{ width:'85%',fontSize: 24, marginLeft: 10, fontWeight: 'bold' }}>{item.product_name}</Text>
+					<Text ellipsizeMode='tail' numberOfLines={1} style={styles.itemTitle}>{item.product_name}</Text>
 					<Text style={{ fontSize: 20, marginTop: 5, marginLeft: 10, }}>Giá: {TotalPrice(item.price, item.quantity)}</Text>
 					<View style={{ flexDirection: 'row', marginLeft: 10, }}>
-						<TouchableOpacity onPress={onChangeQuantity('reduce', item)} style={{ marginLeft: 0, height: 36, width: 36, borderRadius: 36 / 2, backgroundColor: '#ececec', marginRight: 5, marginTop: 5, justifyContent: 'center', alignItems: 'center', }}>
+						<TouchableOpacity onPress={onChangeQuantity('reduce', item)} style={styles.buttonItemBack}>
 							<RnIcon name="caret-back-outline" size={25} color="black" style={{ marginRight: 3 }} />
 						</TouchableOpacity>
 						<Text style={{ marginLeft: 10, fontSize: 20, marginTop: 8 }}>{item.quantity}</Text>
-						<TouchableOpacity onPress={onChangeQuantity('increase', item)} style={{ marginLeft: 15, height: 36, width: 36, borderRadius: 36 / 2, backgroundColor: '#ececec', marginRight: 5, marginTop: 5, justifyContent: 'center', alignItems: 'center', }}>
+						<TouchableOpacity onPress={onChangeQuantity('increase', item)} style={styles.buttonItemForward}>
 							<RnIcon name="caret-forward-outline" size={25} color="black" style={{ marginLeft: 3 }} />
 						</TouchableOpacity>
 					</View>
@@ -87,7 +89,7 @@ export default function Bag({ navigation }) {
 						layout="row"
 					/> */}
 				</View>
-				<View style={{flex:1.2, width: '10%', justifyContent: 'center', alignItems: 'center' }}>
+				<View style={styles.buttonDelete}>
 					<TouchableOpacity onPress={onRemoveItem(item)} style={{}}>
 						<RnIcon name="close-outline" size={40} color="black" />
 					</TouchableOpacity>
@@ -98,10 +100,10 @@ export default function Bag({ navigation }) {
 
 	return (
 		<View style={{ flex: 1, backgroundColor: '#f5f5ef' }}>
-			<View style={{ height: 50, flexDirection: 'row', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center', borderBottomWidth: 2, borderColor: '#f5f5ef' }}>
+			<View style={styles.headerView}>
 				<Text style={{ fontSize: 22, fontWeight: 'bold', marginLeft: 10 }}>Your Bag</Text>
 			</View>
-			<TouchableOpacity onPress={() => { navigation.goBack() }} style={{ position: 'absolute', left: 0, top: 5 }}>
+			<TouchableOpacity onPress={() => { navigation.goBack() }} style={styles.buttonBack}>
 				<RnIcon name="chevron-back-outline" size={36} color="black" />
 			</TouchableOpacity>
 			<SafeAreaView style={{ flex: 1 }}>
@@ -115,8 +117,8 @@ export default function Bag({ navigation }) {
 						ListFooterComponent={
 							<View>
 								{productList?.length ?
-									<TouchableOpacity onPress={onRemoveAll} style={{ marginHorizontal: 50, backgroundColor: '#ff8c70', marginVertical: 10, justifyContent: 'center', alignItems: 'center', height: 50, borderRadius: 25 }}>
-										<View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+									<TouchableOpacity onPress={onRemoveAll} style={styles.buttonRemove}>
+										<View style={styles.viewRemove}>
 											<RnIcon name="trash-outline" size={32} color="black" />
 											<Text style={{ fontSize: 24, marginLeft: 10}}>Remove All</Text>
 										</View>
@@ -126,12 +128,12 @@ export default function Bag({ navigation }) {
 					/>
 				</View>
 				<View style={{flex: 1}}>
-					{productList?.length ? <View style={{ height: '100%', paddingHorizontal: 15, paddingVertical: 15, borderTopWidth: 0.5, borderColor: '#ececec', backgroundColor: 'white' }}>
-						<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+					{productList?.length ? <View style={styles.viewTotal}>
+						<View style={styles.view1}>
 							<Text style={{ fontSize: 22, fontWeight: 'bold' }}>Total item: </Text>
 							<Text style={{ fontSize: 22, fontWeight: 'bold' }}>{totalItem}</Text>
 						</View>
-						<View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+						<View style={styles.view2}>
 							<Text style={{ fontSize: 22, fontWeight: 'bold' }}>Total money: </Text>
 							<Text style={{ fontSize: 22, fontWeight: 'bold' }}>{totalMoney}đ</Text>
 						</View>
@@ -141,3 +143,95 @@ export default function Bag({ navigation }) {
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+    // header
+	headerView: { 
+		height: 50, 
+		flexDirection: 'row', 
+		backgroundColor: 'white' 
+	},
+	buttonBack: { 
+		position: 'absolute', 
+		left: 0, 
+		top: 5 
+	},
+	//
+	buttonRemove: { 
+		marginHorizontal: 50, 
+		backgroundColor: '#ff8c70', 
+		marginVertical: 10, 
+		justifyContent: 'center', 
+		alignItems: 'center', 
+		height: 50, 
+		borderRadius: 25 
+	},
+	viewRemove: { 
+		flexDirection: 'row', 
+		justifyContent: 'center', 
+		alignItems: 'center' 
+	},
+	view1: { 
+		flexDirection: 'row', 
+		justifyContent: 'space-between' 
+	},
+	view2:{ 
+		flexDirection: 'row', 
+		justifyContent: 'space-between', 
+		marginTop: 10 
+	},
+	viewTotal: { 
+		height: '100%', 
+		paddingHorizontal: 15, 
+		paddingVertical: 15, 
+		borderTopWidth: 0.5, 
+		borderColor: '#ececec', 
+		backgroundColor: 'white' 
+	},
+	//render
+	viewItem: { 
+		flex: 1, 
+		marginVertical: 5,
+		marginHorizontal:10, 
+		flexDirection: 'row', 
+		borderRadius: 8, 
+		backgroundColor: 'white', 
+		padding: 10, 
+		width: '95%', 
+		marginRight: 10 
+	},
+	itemTitle: { 
+		width:'85%',
+		fontSize: 24, 
+		marginLeft: 10, 
+		fontWeight: 'bold' 
+	},
+	buttonItemBack: { 
+		marginLeft: 0, 
+		height: 36, 
+		width: 36, 
+		borderRadius: 36 / 2, 
+		backgroundColor: '#ececec', 
+		marginRight: 5, 
+		marginTop: 5, 
+		justifyContent: 'center', 
+		alignItems: 'center', 
+	},
+	buttonItemForward: { 
+		marginLeft: 15, 
+		height: 36, 
+		width: 36, 
+		borderRadius: 36 / 2, 
+		backgroundColor: '#ececec', 
+		marginRight: 5, 
+		marginTop: 5, 
+		justifyContent: 'center', 
+		alignItems: 'center', 
+	},
+	buttonDelete: {
+		flex:1.2, 
+		width: '10%', 
+		justifyContent: 'center', 
+		alignItems: 'center' 
+	},
+});
